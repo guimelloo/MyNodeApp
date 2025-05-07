27f3b57eebc8e0f74b66d1e4141ee3e0466975a4
Itor Isaias <itor.isaias@gmail.com>
2025-05-07 20:39:23 +0200

    Add REST Client extension recommendation, enhance error handling, and update server configuration with examples zod

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,53 @@
 import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
+import { z, ZodError } from 'zod';
 
+const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/todos', async (req, res) => {
-    const { name, date, location, description } = req.body;
+app.post('/api/todos', async (req, res, next) => {
+    try {
+        const schema = z.object({
+            name: z.string().min(1),
+            date: z.coerce.date(),
+            location: z.string().optional(),
+            description: z.string().min(1),
+        });
 
-    const userSchema = z.object({
-        name: z.string().min(1),
-        date: z.coerce.date(),
-        location: z.string().optional(),
-        description: z.string().min(1),
-    })
+        const data = schema.parse(req.body);
+        
+        const todo = await prisma.toDo.create({ data });
 
-    const success  = userSchema.safeParse(req.body).success;
-
-    if (!success) {
-        return res.status(400).json({ error: 'Invalid request body' });
+        res.status(201).header('Location', `/api/todos/${todo.id}`);
+    } catch (error) {
+        next(error);
     }
-
-    const todo = await prisma.toDo.create({
-        data: {
-            name, date, location, description
-        }
-    });
 });
 
-app.get('/api/todos', async (req, res) => {
-    const todos = await prisma.toDo.findMany();
-    res.json(todos);
+app.get('/api/todos', async (req, res, next) => {
+    try {
+        const schema = z.object({
+            per_page: z.coerce.number().min(1).max(100).default(10),
+            page: z.coerce.number().min(1).default(1),
+        });
+
+        const { per_page, page } = schema.parse(req.query);
+
+        const todos = await prisma.toDo.findMany({
+            skip: (page - 1) * per_page,
+            take: per_page,
+        });
+        
+        res.json(todos);
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.delete('/api/todos/:id', async (req, res) => {
@@ -55,7 +68,18 @@ app.patch('/api/todos/:id', async (req, res) => {
     res.json({ message: 'Todo updated' });
 });
 
+app.use((err, req, res, next) => {
+    if (err instanceof ZodError) {
+        return res.status(422).json({ error: err.message });
+    }
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    if (!isProduction) {
+        return res.status(500).json({ error: err.stack });
+    }
+
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
